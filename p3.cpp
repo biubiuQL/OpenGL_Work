835b81762bbde6da7a229276bc7daadb10e21b9f
DESKTOP-34FVBVG\lirong <2229608151@qq.com>
2024-06-17 22:47:46 +0800

    提交

--- p3.cpp
+++ p3.cpp
@@ -42,20 +42,22 @@ int main()
     }
 
     float vertices[] = {
-// 位置              // 颜色
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+        // 位置              // 颜色
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // 右上
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f // 左下
+                                            - 0.5,
+        -0.5f,0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 左上
     };
 
-    // unsigned int indices[] = {
-    //     // 注意索引从0开始!
-    //     // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
-    //     // 这样可以由下标代表顶点组合成矩形
+    unsigned int indices[] = {
+        // 注意索引从0开始!
+        // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+        // 这样可以由下标代表顶点组合成矩形
 
-    //     0, 1, 3, // 第一个三角形
-    //     1, 2, 3  // 第二个三角形
-    // };
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
 
     // 创建顶点缓冲对象
     unsigned int VBO, VAO, EBO;
@@ -70,6 +72,9 @@ int main()
     // 向缓冲对象中写入数据
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // 配置顶点属性：设置如何读取顶点属性，也就是顶点属性的内存布局
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 6 * sizeof(float), (void *)0);
     // 激活顶点属性数组
@@ -80,12 +85,9 @@ int main()
     // 激活顶点属性数组
     glEnableVertexAttribArray(1);
 
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 #pragma region 着色器加载
 
-    std::string rootPath = "C:\\Users\\admin\\Documents\\OpenGL_Work\\shader\\";
+    std::string rootPath = "C:\\Users\\lirong\\Documents\\OpenGL_Work\\shader\\";
     string vertexPath = rootPath + "Vertex.vs";
     string fragmentPath = rootPath + "Fragment.fs";
     Shader shader(vertexPath.c_str(), fragmentPath.c_str());
@@ -105,16 +107,15 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        shader.setGlUniform4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+        // float timeValue = glfwGetTime();
+        // float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        // shader.setGlUniform4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
 
         // 使用渲染数组
-        // glDrawArrays(GL_TRIANGLES, 0, 6);
+        //  glDrawArrays(GL_TRIANGLES, 0, 3);
 
-         glDrawArrays(GL_TRIANGLES, 0, 3);
         // 使用渲染元素
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
